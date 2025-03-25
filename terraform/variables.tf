aea1218d844cb1171c1d9c44514dc13dd9a5d06e
Philipp <philipp.peti@hotmail.com>
2025-03-25 13:13:14 +0100

    feat: added terraform usage

---
+++ terraform/variables.tf
@@ -0,0 +1,25 @@
+variable "subscription_id" {
+  description = "Azure subscription ID"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Azure tenant ID"
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for resources"
+  type        = string
+  default     = "East US"
+}
+
+variable "storage_account_name" {
+  description = "Globally unique name for the storage account"
+  type        = string
+}
